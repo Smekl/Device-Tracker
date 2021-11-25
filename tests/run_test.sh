36d970043e6e3b9d44ac7ebdfae4133ade127e2e
Ido Amiram <ido098@gmail.com>
2021-11-25 12:06:20 +0200

    trying to fix armv7

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -8,5 +8,5 @@ cd ../
 mkdir -p /tmp/data
 cp options.json /tmp/data
 
-sudo docker build -t $DOCKER_NAME -f tests/Dockerfile .
+sudo docker build -t $DOCKER_NAME --build-arg BUILD_FROM=homeassistant/amd64-base:latest -f tests/Dockerfile .
 sudo docker run --network host -v /tmp/data:/data -t -i $DOCKER_NAME sh
