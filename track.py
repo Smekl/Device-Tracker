c25becffdec072db05c9396ac1041fe9e6114caf
Ido <ido098@gmail.com>
2022-10-13 12:51:45 +0300

    fixed debug code

--- track.py
+++ track.py
@@ -89,7 +89,6 @@ def get_args():
     parser = argparse.ArgumentParser(description='Track devices in the network')
     parser.add_argument('--config', dest='config', action='store', help='path to config file')
     parser.add_argument('--token', dest='token', action='store', help='token used to communicate with Home Assistant')
-    parser.add_argument('--debug', dest='debug', action='store_true', help='should we use debug logs', default=False)
 
     args = parser.parse_args()
 
@@ -106,11 +105,11 @@ def main():
     # parse args
     args = get_args()
 
-    # set logging level
-    setup_logging(args.debug)    
-
     # parse config
     config = load_config(args.config)
+
+    # set logging level
+    setup_logging(config['debug'])
 
     # run tracker
     tracker = Tracker(config, args.token)
